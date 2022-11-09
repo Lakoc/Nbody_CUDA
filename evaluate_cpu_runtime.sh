da80f2993d7f0b0197ff927dc6c17c5464545381
Lakoc <xpolok03@stud.fit.vutbr.cz>
2022-11-10 00:41:12 +0100

    CPU runtime

--- evaluate_cpu_runtime.sh
+++ evaluate_cpu_runtime.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-PROJECT_DIR="/home/lakoc/PCG_NBODY"
-
-CPU_DIR="${PROJECT_DIR}/${CPU}"
-
 SIZE_1=1024
 N_START="N=2048"
 
-cd $CPU_DIR
+cd /home/lakoc/PCG_NBODY/CPU
 for i in $(seq 1 10); do
   N=$((2 ** i * SIZE_1))
   sed -i "/${N_START}/c\\N=${N}" Makefile
