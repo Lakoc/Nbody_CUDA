41cd94a74c610d4467e400b5012b789923ebcefb
Lakoc <xpolok03@stud.fit.vutbr.cz>
2022-11-10 10:00:45 +0100

    CPU runtime added

--- evaluate_gpu_runtime.sh
+++ evaluate_gpu_runtime.sh
@@ -14,32 +14,7 @@ SAMPLE_OUTPUT=aaaa
 STEP=$1
 
 
-PROFILE=/apps/all/CUDA/11.7.0/nsight-compute-2022.2.0/target/linux-desktop-glibc_2_11_3-x64/ncu --force-overwrite \
-		--target-processes application-only \
-		--replay-mode kernel \
-		--kernel-name-base function \
-		--launch-skip-before-match 0 \
-		--section ComputeWorkloadAnalysis \
-		--section InstructionStats \
-		--section LaunchStats \
-		--section MemoryWorkloadAnalysis \
-		--section MemoryWorkloadAnalysis_Chart \
-		--section MemoryWorkloadAnalysis_Tables \
-		--section Occupancy \
-		--section SchedulerStats \
-		--section SourceCounters \
-		--section SpeedOfLight \
-		--section SpeedOfLight_RooflineChart \
-		--section WarpStateStats \
-		--sampling-interval auto \
-		--sampling-max-passes 5 \
-		--sampling-buffer-size 33554432 \
-		--profile-from-start 1 \
-		--cache-control all \
-		--clock-control base \
-		--apply-rules yes \
-		--check-exit-code yes
-
+PROFILE="/apps/all/CUDA/11.7.0/nsight-compute-2022.2.0/target/linux-desktop-glibc_2_11_3-x64/ncu --force-overwrite --target-processes application-only --replay-mode kernel --kernel-name-base function --launch-skip-before-match 0 --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_RooflineChart --section WarpStateStats --sampling-interval auto --sampling-max-passes 5 --sampling-buffer-size 33554432 --profile-from-start 1 --cache-control all --clock-control base --apply-rules yes --check-exit-code yes"
 
 for i in $(seq 1 10); do
   N=$((SIZE_1 ** i * SIZE_2))
